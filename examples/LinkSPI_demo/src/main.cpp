9bc88c9dd56651a6b86a8524e460ecc39f6fbd0d
Rodrigo Alfonso <rodri042@gmail.com>
2025-01-03 07:41:16 -0300

    LinkSPI_demo: Adding 2Mbps mode

--- examples/LinkSPI_demo/src/main.cpp
+++ examples/LinkSPI_demo/src/main.cpp
@@ -36,20 +36,22 @@ int main() {
 
     if (!linkSPI->isActive()) {
       firstTransfer = true;
-      output += "START: Set as Master\n";
+      output += "START: Set as Master 256Kbps\n";
       output += "SELECT: Set as Slave\n";
+      output += "RIGHT: Set as Master 2Mbps\n";
       output += "\n(stop: press L+R)\n";
       output += "(hold A on start for async)\n";
       output += "(hold B on start for waitMode)\n";
       output += "(hold UP for 8-bit mode)\n";
       output +=
-          "\n\n\n\n\n\n\n\n\n[!] to test this demo...\n      "
+          "\n\n\n\n\n\n\n\n[!] to test this demo...\n      "
           "...use a GBC Link Cable!";
 
-      if ((keys & KEY_START) | (keys & KEY_SELECT)) {
+      if ((keys & KEY_START) | (keys & KEY_SELECT) | (keys & KEY_RIGHT)) {
         // (3) Initialize the library
-        linkSPI->activate((keys & KEY_START) ? LinkSPI::Mode::MASTER_256KBPS
-                                             : LinkSPI::Mode::SLAVE,
+        linkSPI->activate((keys & KEY_START)    ? LinkSPI::Mode::MASTER_256KBPS
+                          : (keys & KEY_SELECT) ? LinkSPI::Mode::SLAVE
+                                                : LinkSPI::Mode::MASTER_2MBPS,
                           (keys & KEY_UP) ? LinkSPI::DataSize::SIZE_8BIT
                                           : LinkSPI::DataSize::SIZE_32BIT);
         linkSPI->setWaitModeActive(keys &
