25238c4ef453ff78ce8f4051577cd3df01c3545d
Rodrigo Alfonso <rodri042@gmail.com>
2025-02-17 10:05:13 -0300

    Fixing japanese

--- examples/LinkCard_demo/#loader/localize.js
+++ examples/LinkCard_demo/#loader/localize.js
@@ -1,28 +1,25 @@
 const iconv = require('iconv-lite'); // "^0.6.3"
 
-function toFullWidth(str) {
-  const map = {
-    'A': 'Ａ',
-    'B': 'Ｂ',
-    '(': '（',
-    ')': '）',
-    '1': '１',
-    '2': '２',
-    '/': '／'
-  };
-  return str.replace(/[AB\(\)1\/2]/g, ch => map[ch] || ch);
-}
-
 const data = {
-  "MSG_WAITING_GAME": "ゲームを待っています..."
+  "MSG_WAITING_NETWORK": "ネットワークを待っています",
+  "MSG_HANDSHAKE": "ハンドシェイクを実行中",
+  "MSG_REQUESTING_COMMAND": "コマンドを要求中",
+  "MSG_CONFIRMING_REQUEST": "要求の確認",
+  "MSG_SCAN_CARD": "カードをスキャンしてください",
+  "MSG_STARTING_TRANSFER": "転送を開始中",
+  "MSG_SENDING_BYTES": "バイトを送信中",
+  "MSG_CARD_SENT": "カードが送信されました",
+  "MSG_ERROR": "エラー",
+  "MSG_PRESS_A_TRY_AGAIN": "エーを押して再試行",
+  "MSG_PRESS_B_CANCEL": "ビーを押してキャンセル"
 };
 
 for (const key in data) {
-  const fixedStr = toFullWidth(data[key]);
-  const buf = iconv.encode(fixedStr, 'Shift_JIS');
+  const str = data[key];
+  const buf = iconv.encode(str, 'Shift_JIS');
   const hexBytes = Array.from(buf).map(byte => '0x' + byte.toString(16).padStart(2, '0'));
   hexBytes.push("0x00");
-  console.log(`/* "${fixedStr}" */`);
+  console.log(`/* "${str}" */`);
   console.log(`const u8 ${key}[] = { ${hexBytes.join(', ')} };`);
   console.log('');
 }
